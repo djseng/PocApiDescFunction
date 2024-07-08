31e31eb8abb1d489077c9db83989985d9a5a07de
David Seng <dseng@3cloudsolutions.com>
2024-07-08 12:57:27 -0500

    poc works locally

--- HttpDescMe.cs
+++ HttpDescMe.cs
@@ -3,22 +3,24 @@ using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace PocApiDescFunction
+namespace PocApiDescFunction;
+
+public class HttpDescMe
 {
-    public class HttpDescMe
+    private readonly ILogger<HttpDescMe> _logger;
+
+    public HttpDescMe(ILogger<HttpDescMe> logger)
     {
-        private readonly ILogger<HttpDescMe> _logger;
+        _logger = logger;
+    }
 
-        public HttpDescMe(ILogger<HttpDescMe> logger)
-        {
-            _logger = logger;
-        }
-
-        [Function("HttpDescMe")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
-        }
+    [Function("HttpDescMe")]
+    public IActionResult Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post",
+            Route = "hello/{name:alpha}")]
+        HttpRequest req, string name)
+    {
+        _logger.LogInformation("Going to say hello to {name}.", name);
+        return new OkObjectResult(new { message = $"Hello, {name}!"});
     }
 }
